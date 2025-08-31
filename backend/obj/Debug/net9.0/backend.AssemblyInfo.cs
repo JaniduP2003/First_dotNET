bd3f57006a775fa9743ac0ccf6d7207834ab8ee4
Janidu Pabasara <141099578+JaniduP2003@users.noreply.github.com>
2025-08-31 09:34:34 +0530

    first build is success

--- backend/obj/Debug/net9.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net9.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7710cbb455fbdba9843a5dca54e3eb80ff1c1427")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e7157620322f4d0247e41faed128534b4f7d2bb")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
