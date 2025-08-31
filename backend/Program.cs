d87920a61865859b28783d9f657760676d724f40
Janidu Pabasara <141099578+JaniduP2003@users.noreply.github.com>
2025-08-31 16:28:29 +0530

    build failed
       backend   git:(main)  dotnet run
    Using launch settings from /home/janidu/Documents/GitHub/First_dotNET/backend/Properties/launchSettings.json...
    Building...
    /home/janidu/Documents/GitHub/First_dotNET/backend/Program.cs(19,27): error CS0103: The name 'games' does not exist in the current context
    
    The build failed. Fix the build errors and run again.

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,6 +1,22 @@
+using backend.Api.dto;
+
 var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
+
+var list = new List<backendDto>
+{
+    new backendDto(1, "Game One", "Action", 59.99m,
+    DateOnly.FromDateTime(new DateTime(2023, 1, 15))),
+
+    new backendDto(2, "Game Two", "RPG", 49.99m,
+    DateOnly.FromDateTime(new DateTime(2022, 10, 5))),
+
+    new backendDto(3, "Game Three", "Strategy", 39.99m,
+    DateOnly.FromDateTime(new DateTime(2023, 3, 22))),
+};
+
+app.MapGet("games", () => games);
 
 // Simple endpoint that returns "Hello World!"
 app.MapGet("/janidu", () => "Hello from backend!");
