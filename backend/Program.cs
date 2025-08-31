893f5c5ff0c759192ecaee5cce207fdeec83e07f
Janidu Pabasara <141099578+JaniduP2003@users.noreply.github.com>
2025-08-31 15:59:43 +0530

    simple get test check for backend
    success

--- backend/Program.cs
+++ backend/Program.cs
@@ -3,6 +3,6 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 // Simple endpoint that returns "Hello World!"
-app.MapGet("/hello", () => "Hello from backend!");
+app.MapGet("/janidu", () => "Hello from backend!");
 
 app.Run();
