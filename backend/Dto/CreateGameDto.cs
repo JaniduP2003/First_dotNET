6c7413baaa5f80a520198377fa3bbc662fd1abd9
Janidu Pabasara <141099578+JaniduP2003@users.noreply.github.com>
2025-09-04 18:55:06 +0530

    ADD: validations and installed minim.api.exte
    dotnet add package MinimalApis.Extensions --version 0.11.0

--- backend/Dto/CreateGameDto.cs
+++ backend/Dto/CreateGameDto.cs
@@ -1,10 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace backend.Dto;
 
 public record class CreateGameDto(
     //  int id, removed the id becose it is give by the system
     
-    string Name,
-    string Genre,
-    decimal Price,
+    [Required][StringLength(50)]string Name,
+    [Required][StringLength(20)]string Genre,
+    [Range(0,100)]decimal Price,
     DateOnly ReleaseDate
 );
