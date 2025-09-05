3938a02811e91a60e02b4409de38e5780bc11698
Janidu Pabasara <141099578+JaniduP2003@users.noreply.github.com>
2025-09-05 15:59:25 +0530

    it did not crate the table so rall back and update aginn
    Why Database.Migrate() didn’t recreate the table
    
    Database.Migrate() only applies pending migrations.
    
    EF Core does not check the actual database schema, only its migration history table.
    
    Manual deletion of tables does not mark a migration as pending, so it is skipped.
    
    dotnet ef database update 0
    dotnet ef database update

--- backend/obj/Debug/net9.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net9.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65b85a4b0dff04094da3d03167a4f86f30714342")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4fde0373350047afe8508f18f6547fae784acdc2")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
