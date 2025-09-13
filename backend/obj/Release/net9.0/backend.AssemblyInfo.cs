835a01e0db46c1478ad712b66fc8045bd7fff105
Janidu Pabasara <141099578+JaniduP2003@users.noreply.github.com>
2025-09-13 14:35:45 +0530

    Add ::: docker compose for both database and forontend | Error ::ConnectionString property has not been initialized"
    Databse is working fine but back end dont connect to it
    
    in the program.cs
    builder.Services.AddDbContext<GameDataContext>(options =>
        options.UseNpgsql(builder.Configuration.GetConnectionString("PostgresConnection")));   //coments on git
    the name is postgress connection BUT
    
    in the docker.yml in environment: is defultconnction its wrong name mi match

--- backend/obj/Release/net9.0/backend.AssemblyInfo.cs
+++ backend/obj/Release/net9.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f49ee5eaa699be6bceb58da110918e5dc90415f4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+169698550c8eb1a7262fb0bc9ebfa323b25da779")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
